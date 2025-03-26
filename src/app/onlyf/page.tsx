eb26086d0628d041c3c91d5928b573ba830c25ca
Sumit Sabarwal <sumitsabarwal5@gmail.com>
2025-03-26 15:57:33 +0530

    fixed image error

--- src/app/onlyf/page.tsx
+++ src/app/onlyf/page.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Image from "next/image";
 
-export default function page() {
+export default function Page() {
   return (
     <>
-      <section className="my-28 ">
-        <h1 className="text-4xl md:text-7xl font-bold text-center mt-12 mb-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 ">
+      <section className="my-28">
+        <h1 className="text-4xl md:text-7xl font-bold text-center mt-12 mb-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Hell naw!
         </h1>
         <div className="flex justify-center mx-5">
-          <Image
-            className="rounded-xl "
+          <img
+            className="rounded-xl"
             width={600}
             height={600}
-            src={"/dog.png "}
+            src="/dog.png"
             alt="heli naw!"
           />
         </div>
